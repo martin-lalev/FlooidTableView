f19efdf1697328f174e727cf3e4a03b9c647bd6d
Martin Lalev <martin.laleff@gmail.com>
2018-06-13 06:30:23 +0300

    Initial Commit

---
+++ FlooidTableView/FlooidTableView.h
@@ -0,0 +1,19 @@
+//
+//  FlooidTableView.h
+//  FlooidTableView
+//
+//  Created by Martin Lalev on 13.06.18.
+//  Copyright © 2018 Martin Lalev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FlooidTableView.
+FOUNDATION_EXPORT double FlooidTableViewVersionNumber;
+
+//! Project version string for FlooidTableView.
+FOUNDATION_EXPORT const unsigned char FlooidTableViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlooidTableView/PublicHeader.h>
+
+
