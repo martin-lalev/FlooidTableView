a06867643c88054f3bc5277bb7b1a20216fb0d84
Your Name <you@example.com>
2019-06-02 13:26:35 +0300

    TableViewAnimator cleanup

--- FlooidTableView/Diff Helper/TableViewAnimator.swift
+++ FlooidTableView/Diff Helper/TableViewAnimator.swift
@@ -22,142 +22,105 @@ public extension TableViewAnimatorDataProvider {
     func reloadCell(in tableView: UITableView, forRowAt indexPath: IndexPath) -> Void {}
     func finishedReloadingCell(in tableView: UITableView, forRowAt indexPath: IndexPath) -> Void {}
 }
+public extension TableViewAnimatorDataProvider {
+    internal func identifiers(in tableView:UITableView) -> AnimatorIdentifiers {
+        var listIdentifiers: AnimatorIdentifiers = ([],[:])
+        
+        for i in 0 ..< (tableView.dataSource?.numberOfSections?(in: tableView) ?? 0) {
+            let sectionIdentifier = self.sectionIdentifier(in: tableView, at: i)
+            listIdentifiers.section.append(sectionIdentifier)
+            listIdentifiers.data[sectionIdentifier] = []
+            
+            for j in 0 ..< (tableView.dataSource?.tableView(tableView, numberOfRowsInSection: i) ?? 0) {
+                let identifier = self.cellIdentifier(in: tableView, at: IndexPath(row: j, section: i))
+                listIdentifiers.data[sectionIdentifier]?.append(identifier)
+            }
+        }
+        
+        return listIdentifiers
+    }
+}
 
 public class TableViewAnimator {
     
     weak var tableView:UITableView!
     weak var dataProvider:TableViewAnimatorDataProvider!
+    var listIdentifiers: AnimatorIdentifiers = ([],[:])
     
     public init(for tableView:UITableView, with provider:TableViewAnimatorDataProvider) {
         self.dataProvider = provider
         self.tableView = tableView
-        self.loadListIdentifiers(in: tableView)
-    }
-    
-    var listIdentifiers: AnimatorIdentifiers = ([],[:])
-    func loadListIdentifiers(in tableView:UITableView) {
-        self.listIdentifiers = ([],[:])
-        
-        for i in 0 ..< (tableView.dataSource?.numberOfSections?(in: tableView) ?? 0) {
-            let sectionIdentifier = self.dataProvider.sectionIdentifier(in: tableView, at: i)
-            self.listIdentifiers.section.append(sectionIdentifier)
-            self.listIdentifiers.data[sectionIdentifier] = []
-            
-            for j in 0 ..< (tableView.dataSource?.tableView(tableView, numberOfRowsInSection: i) ?? 0) {
-                let identifier = self.dataProvider.cellIdentifier(in: tableView, at: IndexPath(row: j, section: i))
-                self.listIdentifiers.data[sectionIdentifier]?.append(identifier)
-            }
-        }
-    }
-    func copyListIdentifiers() -> AnimatorIdentifiers {
-        var identifiers:AnimatorIdentifiers = ([],[:])
-        self.listIdentifiers.section.forEach { identifiers.section.append($0) }
-        self.listIdentifiers.data.forEach { identifiers.data[$0] = $1 }
-        return identifiers
+        self.listIdentifiers = self.dataProvider.identifiers(in: tableView)
     }
     
     private var mustReload = false
-    public func reloadData(with animation:UITableView.RowAnimation = .fade, otherAnimations:@escaping ()->Void = { }, completed:@escaping ()->Void = { }) {
-        
+    private func reloadData(_ completed: @escaping (AnimatorIdentifiers, AnimatorIdentifiers) -> Void) {
         self.mustReload = true
         DispatchQueue.main.async {
             guard self.mustReload else { return }
             self.mustReload = false
             
-            let old:AnimatorIdentifiers = self.copyListIdentifiers()
-            self.loadListIdentifiers(in: self.tableView)
-            let new:AnimatorIdentifiers = self.copyListIdentifiers()
+            let old = self.listIdentifiers
+            self.listIdentifiers = self.dataProvider.identifiers(in: self.tableView)
+            let new = self.listIdentifiers
             
-            self.tableView.animateChanges(from: old, to: new, animation: .fade, otherAnimations: {
+            completed(old, new)
+        }
+    }
+    public func reloadData(with animation: UITableView.RowAnimation = .fade, otherAnimations: @escaping () -> Void = { }, completed: @escaping () -> Void = { }) {
+        self.reloadData { (old, new) in
+            
+            self.tableView.update(changes: {
+                let reloadSections = self.tableView.animateSectionsChanges(from: old.section, to: new.section, rowAnimation: animation)
+                for sectionIdentifier in reloadSections {
+                    self.tableView.animateCellsChanges(in:new.section.firstIndex(of: sectionIdentifier)!, from: old.data[sectionIdentifier]!, to: new.data[sectionIdentifier]!, rowAnimation: animation)
+                }
+                
+            }, animations: {
                 for indexPath in self.tableView.indexPathsForVisibleRows ?? [] {
                     self.dataProvider.reloadCell(in: self.tableView, forRowAt: indexPath)
                 }
                 otherAnimations()
-            }, completed: {
+                
+            }) {
                 for indexPath in self.tableView.indexPathsForVisibleRows ?? [] {
                     self.dataProvider.finishedReloadingCell(in: self.tableView, forRowAt: indexPath)
                 }
                 completed()
-            })
+            }
         }
     }
 }
 
+
+
+
+
 extension UITableView {
-    
-    func animateChanges(from:AnimatorIdentifiers, to:AnimatorIdentifiers, animation:UITableView.RowAnimation = .fade, otherAnimations:@escaping ()->Void = { }, completed:@escaping ()->Void = { }) {
-        
+    func update(changes: () -> Void, animations: @escaping () -> Void, _ completed: @escaping () -> Void = { }) {
         CATransaction.begin()
         CATransaction.setCompletionBlock(completed)
         
-        
         self.beginUpdates()
-        let reloadSections = self.animateChanges(from: from.section, to: to.section, rowAnimation: animation)
-        for sectionIdentifier in reloadSections {
-            self.animateChanges(in: to.section.firstIndex(of: sectionIdentifier)!, from: from.data[sectionIdentifier]!, to: to.data[sectionIdentifier]!, rowAnimation: animation)
-        }
+        changes()
         self.endUpdates()
-        
         
         let duration = CATransaction.animationDuration();
         CATransaction.commit();
         
         UIView.animate(withDuration: duration, delay: 0, options: [.allowUserInteraction], animations: {
-            otherAnimations();
-        }) { _ in }
+            animations()
+        })
         self.beginUpdates()
         self.endUpdates()
-        
+
     }
-    
-//    @discardableResult
-//    func animateChanges(from sections_old:[String], to sections_new:[String], rowAnimation:UITableView.RowAnimation) -> [String] {
-//
-//        let operations = diff(sections_old, sections_new)
-//
-//        var reloadItems:[String] = [];
-//
-//        for operation in operations {
-//            switch operation {
-//            case .delete(let index):
-//                self.deleteSections(IndexSet.init(integer: index), with: rowAnimation)
-//            case .insert(let index):
-//                self.insertSections(IndexSet.init(integer: index), with: rowAnimation)
-//            case .move(let from, let to):
-//                self.moveSection(from, toSection: to)
-//            case .update(let index):
-//                reloadItems.append(sections_new[index])
-//            }
-//        }
-//
-//        return reloadItems;
-//    }
-//
-//    @discardableResult
-//    func animateChanges(in section:Int, from sourceData_old:[String], to sourceData_new:[String], rowAnimation:UITableView.RowAnimation) -> [IndexPath] {
-//
-//        let operations = diff(sourceData_old, sourceData_new)
-//
-//        var reloadIndexPaths:[IndexPath] = []
-//
-//        for operation in operations {
-//            switch operation {
-//            case .delete(let index):
-//                self.deleteRows(at: [IndexPath(row: index, section: section)], with: rowAnimation)
-//            case .insert(let index):
-//                self.insertRows(at: [IndexPath(row: index, section: section)], with: rowAnimation)
-//            case .move(let from, let to):
-//                self.moveRow(at: IndexPath(row: from, section: section), to: IndexPath(row: to, section: section))
-//            case .update(let index):
-//                reloadIndexPaths.append(IndexPath(row: index, section: section))
-//            }
-//        }
-//
-//        return reloadIndexPaths;
-//    }
-    
+}
+
+extension UITableView {
+
     @discardableResult
-    func animateChanges(from sections_old:[String], to sections_new:[String], rowAnimation:UITableView.RowAnimation) -> [String] {
+    func animateSectionsChanges(from sections_old:[String], to sections_new:[String], rowAnimation:UITableView.RowAnimation) -> [String] {
         
         // REMOVE ITEMS
         var removedItems = Set.init(sections_old); removedItems.subtract(sections_new)
@@ -197,8 +160,7 @@ extension UITableView {
         return reloadItems;
     }
     
-    @discardableResult
-    func animateChanges(in section:Int, from sourceData_old:[String], to sourceData_new:[String], rowAnimation:UITableView.RowAnimation) -> [IndexPath] {
+    func animateCellsChanges(in section:Int, from sourceData_old:[String], to sourceData_new:[String], rowAnimation:UITableView.RowAnimation) {
         
         
         // REMOVE ITEMS
@@ -230,12 +192,11 @@ extension UITableView {
             guard let j = sourceData_old.firstIndex(of: sourceData_new[i]) else { continue }
             if i != j {
                 self.moveRow(at: IndexPath(row: j, section: section), to: IndexPath(row: i, section: section))
+            } else {
+                reloadIndexPaths.append(IndexPath(row: i, section: section))
             }
-            reloadIndexPaths.append(IndexPath(row: i, section: section))
         }
-        
-        
-        return reloadIndexPaths;
+//        self.reloadRows(at: reloadIndexPaths, with: rowAnimation)
     }
     
 }
