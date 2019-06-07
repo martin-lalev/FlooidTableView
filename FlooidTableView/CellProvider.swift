4ed6ae47455ce94321b08d51736217637ae76949
Your Name <you@example.com>
2019-06-07 10:53:06 +0300

    Instead of AnyCellProvider, now the CellProvider is implementing the datasource/delegate methods => CellProviders now expect the types which implement it to have a more limited set of methods;

--- FlooidTableView/CellProvider.swift
+++ FlooidTableView/CellProvider.swift
@@ -9,38 +9,111 @@
 import Foundation
 import UIKit
 
-public protocol CellProvider {
-    
-    func dequeue(in tableView: UITableView, at indexPath: IndexPath) -> UITableViewCell
-    
-    func height(in tableView: UITableView, at indexPath: IndexPath) -> CGFloat
-    
-    func estimatedHeight(in tableView: UITableView, at indexPath: IndexPath) -> CGFloat
-    
-    func identifier(in tableView: UITableView, at indexPath: IndexPath) -> String
-    
-    func reload(in tableView: UITableView, at indexPath: IndexPath) -> Void
-    
-    func willShow(_ cell: UITableViewCell, in tableView: UITableView, at indexPath: IndexPath) -> Void
-    
-    func didHide(_ cell: UITableViewCell, in tableView: UITableView, at indexPath: IndexPath) -> Void
-
-}
-
 extension UITableViewCell {
     public static func register(in tableView:UITableView) {
         tableView.register(self, forCellReuseIdentifier: self.description())
     }
 }
 
-public class AnyCellProvider<CellType: UITableViewCell>: CellProvider {
+public protocol CellProvider {
+    
+    var identifier: String { get }
+    var cellIdentifier: String { get }
+
+    func height(tableView: UITableView) -> CGFloat
+    func estimatedHeight(tableView: UITableView) -> CGFloat
+
+    func setup(_ cell: UITableViewCell)
+    func willShow(_ cell: UITableViewCell)
+    func didHide(_ cell: UITableViewCell)
+}
+
+extension CellProvider {
+    public func willShow(_ cell: UITableViewCell) { }
+    public func didHide(_ cell: UITableViewCell) { }
+    public func estimatedHeight(tableView: UITableView) -> CGFloat {
+        return self.height(tableView: tableView)
+    }
+}
+
+extension CellProvider {
+    
+    public func dequeue(in tableView: UITableView, at indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath as IndexPath)
+        self.setup(cell)
+        return cell
+    }
+    
+    public func reload(in tableView: UITableView, at indexPath: IndexPath) -> Void {
+        guard let cell = tableView.cellForRow(at: indexPath) else { return }
+        self.setup(cell)
+    }
+    
+    public func height(in tableView: UITableView, at indexPath: IndexPath) -> CGFloat {
+        return self.height(tableView: tableView)
+    }
+    
+    public func estimatedHeight(in tableView: UITableView, at indexPath: IndexPath) -> CGFloat {
+        return self.estimatedHeight(tableView: tableView)
+    }
+    
+    public func identifier(in tableView: UITableView, at indexPath: IndexPath) -> String {
+        return self.identifier
+    }
+    
+    public func willShow(_ cell: UITableViewCell, in tableView: UITableView, at indexPath: IndexPath) -> Void {
+        self.willShow(cell)
+    }
+    
+    public func didHide(_ cell: UITableViewCell, in tableView: UITableView, at indexPath: IndexPath) -> Void {
+        self.didHide(cell)
+    }
+
+}
+
+
+
+
+
+// Any Cell Provider
+public protocol SpecificCellProvider: CellProvider {
+    associatedtype CellType: UITableViewCell
+    
+    func setup(_ cell: CellType)
+    func willShow(_ cell: CellType)
+    func didHide(_ cell: CellType)
+}
+public extension SpecificCellProvider {
+    var cellIdentifier: String { return CellType.description() }
+    func setup(_ cell: UITableViewCell) {
+        guard let cell = cell as? CellType else { return }
+        self.setup(cell)
+    }
+    func willShow(_ cell: UITableViewCell) {
+        guard let cell = cell as? CellType else { return }
+        self.willShow(cell)
+    }
+    func didHide(_ cell: UITableViewCell) {
+        guard let cell = cell as? CellType else { return }
+        self.didHide(cell)
+    }
+}
+public extension SpecificCellProvider {
+    func willShow(_ cell: CellType) {}
+    func didHide(_ cell: CellType) {}
+}
+
+
+
+// StandardCellProvder
+public class AnyCellProvider<CellType: UITableViewCell>: SpecificCellProvider {
     
     let setup: (CellType)->Void
     let willShow: (CellType)->Void
     let didHide: (CellType)->Void
     let height: (UITableView) -> CGFloat
     let heightEstimation: ((UITableView) -> CGFloat)?
-    let identifier: String
+    public let identifier: String
     
     public init(identifier: String, height: @escaping (UITableView) -> CGFloat, heightEstimation: ((UITableView) -> CGFloat)? = nil, willShow: @escaping (CellType)->Void = { _ in }, didHide: @escaping (CellType)->Void = { _ in }, setup: @escaping (CellType)->Void) {
         self.identifier = identifier
@@ -54,37 +127,20 @@ public class AnyCellProvider<CellType: UITableViewCell>: CellProvider {
         self.init(identifier: identifier, height: { _ in height }, heightEstimation: heightEstimation, willShow: willShow, didHide: didHide, setup: setup)
     }
     
-    public func dequeue(in tableView: UITableView, at indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: CellType.description(), for: indexPath as IndexPath) as! CellType
-        self.setup(cell)
-        return cell
-    }
-    
-    public func reload(in tableView: UITableView, at indexPath: IndexPath) -> Void {
-        guard let cell = tableView.cellForRow(at: indexPath) as? CellType else { return }
-        self.setup(cell)
-    }
-    
-    public func height(in tableView: UITableView, at indexPath: IndexPath) -> CGFloat {
+    public func height(tableView: UITableView) -> CGFloat {
         return self.height(tableView)
     }
-    
-    public func estimatedHeight(in tableView: UITableView, at indexPath: IndexPath) -> CGFloat {
+    public func estimatedHeight(tableView: UITableView) -> CGFloat {
         return self.heightEstimation?(tableView) ?? self.height(tableView)
     }
-    
-    public func identifier(in tableView: UITableView, at indexPath: IndexPath) -> String {
-        return self.identifier
+    public func setup(_ cell: CellType) {
+        return self.setup(cell)
     }
-    
-    public func willShow(_ cell: UITableViewCell, in tableView: UITableView, at indexPath: IndexPath) -> Void {
-        guard let cell = cell as? CellType else { return }
-        self.willShow(cell)
+    public func willShow(_ cell: CellType) {
+        return self.willShow(cell)
     }
-    
-    public func didHide(_ cell: UITableViewCell, in tableView: UITableView, at indexPath: IndexPath) -> Void {
-        guard let cell = cell as? CellType else { return }
-        self.didHide(cell)
+    public func didHide(_ cell: CellType) {
+        return self.didHide(cell)
     }
 
 }
