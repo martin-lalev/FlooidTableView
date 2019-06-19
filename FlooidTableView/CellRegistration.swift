062095259ba5efa84c8d720c8e4e0a6b10e5ed97
Your Name <you@example.com>
2019-06-19 11:03:04 +0300

    Updated the registration structure to better support both code-based and xib-based cells

--- FlooidTableView/CellRegistration.swift
+++ FlooidTableView/CellRegistration.swift
@@ -11,24 +11,31 @@ import UIKit
 
 public protocol IdentifiableTableViewCell: UITableViewCell {
     static var reuseIdentifier: String { get }
+    static func register(in tableView: UITableView)
 }
 
-public extension IdentifiableTableViewCell {
-    static func register(in tableView:UITableView) {
+
+
+public protocol CodebasedCellView: IdentifiableTableViewCell {
+    static var registerableClass: AnyClass? { get }
+}
+
+public extension CodebasedCellView {
+    static var registerableClass: AnyClass? { return self }
+    static func register(in tableView: UITableView) {
         tableView.register(self, forCellReuseIdentifier: self.reuseIdentifier)
     }
 }
 
-public protocol XibbedCellView {
+
+
+public protocol XibbedCellView: IdentifiableTableViewCell {
     static var xibName: String { get }
     static var bundle: Bundle? { get }
 }
 public extension XibbedCellView {
     static var bundle: Bundle? { return .main }
-}
-
-public extension XibbedCellView where Self: IdentifiableTableViewCell {
-    static func register(in tableView:UITableView) {
+    static func register(in tableView: UITableView) {
         tableView.register(UINib(nibName: self.xibName, bundle: self.bundle), forCellReuseIdentifier: self.reuseIdentifier)
     }
 }
